7923e09eb94bcff0fc304f7625d2725050127036
Mark Santcroos <m.a.santcroos@lumc.nl>
2021-09-15 00:24:21 +0200

    Flake8 config.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mutalyzer-algebra",
-    version="0.0.1",
+    version="0.1.0",
     description="A library and CLI for A Boolean Algebra for Genetic Variants.",
     url="https://github.com/mutalyzer/algebra",
     author="Mark Santcroos",
