8e049311a1184167fc9f740640c6326acea2d7cf
Mark Santcroos <m.a.santcroos@lumc.nl>
2022-03-30 11:11:03 +0200

    More proper cli.

--- q.py
+++ q.py
@@ -1,3 +1,4 @@
+import sys
 from algebra.lcs.wupp import edit as edit_test
 from algebra.lcs.efficient import edit as edit_gold
 from pprint import pprint
@@ -15,22 +16,33 @@ def compare(test, gold, f):
                 assert gold[row][col] == test[row][col]
 
 
-min_rand = 1
-max_rand = 2
-lhs = "".join(random.choice("ACGT") for _ in range(random.randint(min_rand, max_rand)))
-rhs = "".join(random.choice("ACGT") for _ in range(random.randint(min_rand, max_rand)))
-lhs = "CA"
-rhs = "T"
-print(lhs, rhs)
+def main():
+    min_rand = 1
+    max_rand = 2
 
-dist_test, matrix_test = edit_test(lhs, rhs)
-dist_gold, _, matrix_gold = edit_gold(lhs, rhs)
+    if len(sys.argv) == 1:
+        lhs = "".join(random.choice("ACGT") for _ in range(random.randint(min_rand, max_rand)))
+        rhs = "".join(random.choice("ACGT") for _ in range(random.randint(min_rand, max_rand)))
+    elif len(sys.argv) == 3:
+        lhs = sys.argv[1]
+        rhs = sys.argv[2]
+    else:
+        raise("usage")
 
-print(dist_test)
-pprint(matrix_test)
-print(dist_gold)
-pprint(matrix_gold)
+    print(lhs, rhs)
 
-assert dist_test == dist_gold
+    dist_test, matrix_test = edit_test(lhs, rhs)
+    dist_gold, _, matrix_gold = edit_gold(lhs, rhs)
 
-compare(matrix_test, matrix_gold, dist_test)
+    print(dist_test)
+    pprint(matrix_test)
+    print(dist_gold)
+    pprint(matrix_gold)
+
+    assert dist_test == dist_gold
+
+    compare(matrix_test, matrix_gold, dist_test)
+
+
+if __name__ == '__main__':
+    main()
