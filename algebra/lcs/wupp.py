dec6e19cb622a4f52035770176ecdc03e1971aec
Mark Santcroos <m.a.santcroos@lumc.nl>
2022-04-05 14:54:17 +0200

    Limit based on position relative to 0 diagonal.

--- algebra/lcs/wupp.py
+++ algebra/lcs/wupp.py
@@ -1,17 +1,24 @@
 def edit(reference, observed):
-    def extend(idx, start, end, value):
-        if idx >= 0:
+    def extend(idx):
+        start = diagonals[offset + idx]
+        if idx > 0:
             row = start
             col = row + idx
-        else:
+            end = max(diagonals[offset + idx - 1] - 1, diagonals[offset + idx + 1])
+        elif idx < 0:
             col = start
             row = col - idx
+            end = max(diagonals[offset + idx - 1], diagonals[offset + idx + 1] - 1)
+        else:
+            row = start
+            col = row + idx
+            end = max(diagonals[offset + idx - 1], diagonals[offset + idx + 1])
 
         steps = 1
         print(f"    start at {row, col}")
         for _ in range(start, end):
             print(f"        implicit {row, col}")
-            matrix[row][col] = value
+            matrix[row][col] = abs(delta) + 2 * it
             row += 1
             col += 1
             steps += 1
@@ -19,31 +26,31 @@ def edit(reference, observed):
         print(f"    continue with {row, col}")
         while row <= len(reference) and col <= len(observed) and reference[row - 1] == observed[col - 1]:
             print(f"        match {row, col}")
-            matrix[row][col] = value
+            matrix[row][col] = abs(delta) + 2 * it
             row += 1
             col += 1
             steps += 1
 
         print(f"    last checked {row, col}")
-        #matrix[row][col] = value + 2
+        matrix[row][col] = abs(delta) + 2 * it + 2
         return start + steps
 
-    matrix = [[None for _ in range(len(observed) + 1)] for _ in range(len(reference) + 1)]
+    matrix = [[None for _ in range(len(observed) + 2)] for _ in range(len(reference) + 2)]
 
     delta = len(observed) - len(reference)
     offset = len(reference) + 1
     diagonals = [1] * (len(reference) + len(observed) + 3)
     it = 0
 
-    while diagonals[offset + delta] <= max(len(reference), len(observed)):
+    while diagonals[offset + delta] <= len(observed) + 1 - delta:
         for idx in range(-it, delta):
             print(f"extend diagonal {idx} (lower)")
-            diagonals[offset + idx] = extend(idx, diagonals[offset + idx], max(diagonals[offset + idx - 1] + 1, diagonals[offset + idx + 1]) - 1, abs(delta) + 2 * it)
+            diagonals[offset + idx] = extend(idx)
         for idx in range(delta + it, delta, -1):
             print(f"extend diagonal {idx} (upper)")
-            diagonals[offset + idx] = extend(idx, diagonals[offset + idx], max(diagonals[offset + idx - 1], diagonals[offset + idx + 1] + 1) - 1, abs(delta) + 2 * it)
+            diagonals[offset + idx] = extend(idx)
         print(f"extend diagonal {delta} (delta)")
-        diagonals[offset + delta] = extend(delta, diagonals[offset + delta], max(diagonals[offset + delta - 1], diagonals[offset + delta + 1]) - delta, abs(delta) + 2 * it)
+        diagonals[offset + delta] = extend(delta)
 
         for row in matrix:
             print(row)
@@ -51,4 +58,4 @@ def edit(reference, observed):
 
         it += 1
 
-    return abs(delta) + 2 * (it - 1), matrix
+    return abs(delta) + 2 * (it - 1), [r[:-1] for r in matrix[:-1]]
