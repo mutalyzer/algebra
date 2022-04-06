bd2c0cc2f5b338e4144b346fe72fc1217412efa2
Mark Santcroos <m.a.santcroos@lumc.nl>
2022-04-06 14:28:36 +0200

    Spit out concatenated ranges and lcs.

--- algebra/lcs/wupp.py
+++ algebra/lcs/wupp.py
@@ -1,4 +1,7 @@
 def edit(reference, observed):
+    def lcs_idx(row, col):
+        return ((row + col) - (abs(delta) + 2 * it - abs((len(reference) - row) - (len(observed) - col)))) // 2 - 1
+
     def extend(idx):
         start = diagonals[offset + idx]
         if idx > 0:
@@ -14,23 +17,44 @@ def edit(reference, observed):
             col = row + idx
             end = max(diagonals[offset + idx - 1], diagonals[offset + idx + 1])
 
-        print(f"    start at {row, col}")
+        # print(f"    start at {row, col}")
+        active = False
+        match_row = 0
+        match_col = 0
         for _ in range(start, end):
-            print(f"        implicit {row, col}")
+            # print(f"        implicit {row, col}")
             matrix[row + 1][col + 1] = abs(delta) + 2 * it
+            if reference[row] == observed[col]:
+                # print(f"{row + 1, col + 1} implicit match: {reference[row]}")
+                if not active:
+                    match_row = row
+                    match_col = col
+                active = True
+            elif active:
+                print(f"{match_row + 1, match_col + 1}, {row + 1, col + 1} match")
+                print('lcs pos', lcs_idx(row, col))
+                active = False
             row += 1
             col += 1
 
-        print(f"    continue with {row, col}")
+        # print(f"    continue with {row, col}")
         steps = end + 1
+        if not active:
+            match_row = row
+            match_col = col
         while row < len(reference) and col < len(observed) and reference[row] == observed[col]:
-            print(f"        match {row, col}")
+            active = True
+            # print(f"        match {row, col}")
             matrix[row + 1][col + 1] = abs(delta) + 2 * it
+            # print(f"{row + 1, col + 1} match: {reference[row]}")
             row += 1
             col += 1
             steps += 1
+        if active:
+            print(f"{match_row + 1, match_col + 1}, {row + 1, col + 1} match")
+            print('lcs pos', lcs_idx(row, col))
 
-        print(f"    last checked {row, col}")
+        # print(f"    last checked {row, col}")
         matrix[row + 1][col + 1] = abs(delta) + 2 * it + 2
         return steps
 
@@ -50,17 +74,17 @@ def edit(reference, observed):
 
     while diagonals[offset + delta] <= max(len(reference), len(observed)) - abs(delta):
         for idx in range(lower - it, delta):
-            print(f"extend diagonal {idx} (lower)")
+            # print(f"extend diagonal {idx} (lower)")
             diagonals[offset + idx] = extend(idx)
         for idx in range(upper + it, delta, - 1):
-            print(f"extend diagonal {idx} (upper)")
+            # print(f"extend diagonal {idx} (upper)")
             diagonals[offset + idx] = extend(idx)
-        print(f"extend diagonal {delta} (delta)")
+        # print(f"extend diagonal {delta} (delta)")
         diagonals[offset + delta] = extend(delta)
 
-        for row in matrix:
-            print(row)
-        print(diagonals)
+        # for row in matrix:
+        #     print(row)
+        # print(diagonals)
 
         it += 1
 
