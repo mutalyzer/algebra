deca4549787fb6521e67e63bc59223a04c6a6178
Mark Santcroos <m.a.santcroos@lumc.nl>
2022-04-11 17:00:47 +0200

    Go back to old "efficient" implementation.

--- algebra/lcs/efficient.py
+++ algebra/lcs/efficient.py
@@ -1,28 +1,39 @@
 from ..variants.variant import Variant
+from itertools import combinations
+
+
+class Node:
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+        self.child = []
+
+    def __str__(self):
+        return f"{self.row, self.col}"
+
+
+def heur_func(reference, observed, row, col):
+    """Heuristic function for A*.
+    Args:
+        reference (string): reference sequence
+        observed (string): observed sequence
+        row (integer): row of the element
+        col (integer): column of the element
+    Returns: the minimal edit distance for the element at (row, col) to
+             the end of both (reference and observed) strings.
+    """
+    return abs((len(reference) - row) - (len(observed) - col))
 
 
 def edit(reference, observed):
-    """Longest Common Subsequence (LCS) edit distance
+    """Calculates all longest common subsequences (LCS).
+    Args:
+        reference (string): reference sequence
+        observed (string): observed sequence
     """
-    def heuristic(row, col):
-        """Heuristic function for A*
-        """
-        return abs((len(reference) - row) - (len(observed) - col))
+    f = heur_func(reference, observed, 0, 0)
 
-    def lcs_match(lcs_nodes, lcs_len, row, col, f):
-        """
-        """
-        distance = f - heuristic(row, col)
-        lcs_pos = ((row + col) - distance) // 2 - 1
-        length = lcs_nodes[lcs_pos - 1].pop((row - 1, col - 1), {"length": 0})["length"]
-        lcs_nodes[lcs_pos][(row, col)] = {"length": length + 1}
-        return max(lcs_len, lcs_pos + 1)
-
-    matrix = [[None for _ in range(len(observed) + 1)]
-              for _ in range(len(reference) + 1)]
-    f = heuristic(0, 0)
-
-    lcs_nodes = [{} for _ in range(min(len(reference), len(observed)))]
+    graph = [[] for _ in range(min(len(reference), len(observed)))]
     lcs_len = 0
 
     rows = [1] + [-1] * len(reference)
@@ -46,15 +57,31 @@ def edit(reference, observed):
 
             if row > 0 and reference[row - 1] == observed[col - 1]:
                 if rows[row - 1] + offset > col - 1:
-                    lcs_len = lcs_match(lcs_nodes, lcs_len, row, col, f)
-                    matrix[row][col] = f
+                    distance = f - heur_func(reference, observed, row, col)
+                    lcs_pos = ((row + col) - distance) // 2 - 1
+                    graph[lcs_pos].append(Node(row, col))
+                    lcs_len = max(lcs_len, lcs_pos + 1)
                 elif row == row_start:
-                    lcs_len = lcs_match(lcs_nodes, lcs_len, row, col, f + 2)
-                    matrix[row][col] = f + 2
+                    distance = (f + 2) - heur_func(reference, observed, row, col)
+                    lcs_pos = ((row + col) - distance) // 2 - 1
+                    graph[lcs_pos].append(Node(row, col))
+                    lcs_len = max(lcs_len, lcs_pos + 1)
 
-            if (heuristic(row, col) < heuristic(row, col - 1) or
-                    (row > 0 and rows[row - 1] + offset > col and heuristic(row, col) < heuristic(row - 1, col)) or
-                    (row > 0 and rows[row - 1] + offset > col - 1 and reference[row - 1] == observed[col - 1])):
+            if (
+                heur_func(reference, observed, row, col)
+                < heur_func(reference, observed, row, col - 1)
+                or (
+                    row > 0
+                    and rows[row - 1] + offset > col
+                    and heur_func(reference, observed, row, col)
+                    < heur_func(reference, observed, row - 1, col)
+                )
+                or (
+                    row > 0
+                    and rows[row - 1] + offset > col - 1
+                    and reference[row - 1] == observed[col - 1]
+                )
+            ):
                 rows[row] += 1
                 cols[col] = max(cols[col] + offset, row + 1) - offset
                 col_end = max(col_end, col + 1)
@@ -72,15 +99,31 @@ def edit(reference, observed):
 
             if col > 0 and reference[row - 1] == observed[col - 1]:
                 if cols[col - 1] + offset > row - 1:
-                    lcs_len = lcs_match(lcs_nodes, lcs_len, row, col, f)
-                    matrix[row][col] = f
+                    distance = f - heur_func(reference, observed, row, col)
+                    lcs_pos = ((row + col) - distance) // 2 - 1
+                    graph[lcs_pos].append(Node(row, col))
+                    lcs_len = max(lcs_len, lcs_pos + 1)
                 elif col == col_start:
-                    lcs_len = lcs_match(lcs_nodes, lcs_len, row, col, f + 2)
-                    matrix[row][col] = f + 2
+                    distance = (f + 2) - heur_func(reference, observed, row, col)
+                    lcs_pos = ((row + col) - distance) // 2 - 1
+                    graph[lcs_pos].append(Node(row, col))
+                    lcs_len = max(lcs_len, lcs_pos + 1)
 
-            if (heuristic(row, col) < heuristic(row - 1, col) or
-                    (col > 0 and cols[col - 1] + offset > row and heuristic(row, col) < heuristic(row, col - 1)) or
-                    (col > 0 and cols[col - 1] + offset > row - 1 and reference[row - 1] == observed[col - 1])):
+            if (
+                heur_func(reference, observed, row, col)
+                < heur_func(reference, observed, row - 1, col)
+                or (
+                    col > 0
+                    and cols[col - 1] + offset > row
+                    and heur_func(reference, observed, row, col)
+                    < heur_func(reference, observed, row, col - 1)
+                )
+                or (
+                    col > 0
+                    and cols[col - 1] + offset > row - 1
+                    and reference[row - 1] == observed[col - 1]
+                )
+            ):
                 cols[col] += 1
                 rows[row] = max(rows[row] + offset, col + 1) - offset
                 row_end = max(row_end, row + 1)
@@ -93,8 +136,12 @@ def edit(reference, observed):
 
         if row_start == row_end and col_start == col_end:
             if reference[row_end - 1] == observed[col_end - 1]:
-                lcs_len = lcs_match(lcs_nodes, lcs_len, row_end, col_end, f)
-                matrix[row_end][col_end] = f
+                lcs_pos = (
+                    (row_end + col_end)
+                    - (f - heur_func(reference, observed, row_end, col_end))
+                ) // 2 - 1
+                graph[lcs_pos].append(Node(row_end, col_end))
+                lcs_len = max(lcs_len, lcs_pos + 1)
 
                 row_end += 1
                 col_end += 1
@@ -112,21 +159,158 @@ def edit(reference, observed):
             row_end += 1
             col_end += 1
 
-    return f, lcs_nodes[:lcs_len], matrix
+    return f, graph[:lcs_len]
 
 
-def lcs_graph(reference, observed, lcs_nodes):
-    if lcs_nodes == []:
-        return {}
+def manhattan(observed, row_start, col_start, row_end, col_end):
+    ops = set()
 
-    graph = {(len(reference) + 1, len(observed) + 1): list(lcs_nodes[-1].keys())}
-    for level in range(len(lcs_nodes) - 1, 0, -1):
-        for to_node, to_length in lcs_nodes[level].items():
-            for length in range(to_length):
-                for from_node, from_length in lcs_nodes[level - length - 1].items():
-                    if from_node[0] < to_node[0] - length and from_node[1] < to_node[1] - length:
-                        print(f"edge {from_node}:{from_length} -> {to_node}:{to_length}")
-                        from_node = True
+    for row in range(row_start + 1, row_end):
+        ops.add((row, "del"))
 
-    print(lcs_nodes)
-    return graph
+    for row in range(row_start, row_end):
+        for col in range(col_start, col_end - 1):
+            ops.add((row, "ins", observed[col]))
+
+    return ops
+
+
+def build(graph, reference, observed):
+    ops = set()
+
+    if graph == []:
+        return manhattan(observed, 0, 0, len(reference) + 1, len(observed) + 1), graph
+
+    # all nodes on the last level are reachable and they indicate the
+    # end of the LCS
+    for node in graph[len(graph) - 1]:
+        ops.update(
+            manhattan(
+                observed, node.row, node.col, len(reference) + 1, len(observed) + 1
+            )
+        )
+        node.child = [None]
+
+    # construct the LCSs from the last match to the first match
+    for level in range(len(graph), 1, -1):
+        # unPythonic allows for removal of nodes while iterating
+        idx = 0
+        while idx < len(graph[level - 1]):
+            node = graph[level - 1][idx]
+            if len(node.child) > 0:
+                for prev in graph[level - 2]:
+                    if prev.row < node.row and prev.col < node.col:
+                        ops.update(
+                            manhattan(observed, prev.row, prev.col, node.row, node.col)
+                        )
+                        prev.child.append(node)
+                idx += 1
+            else:
+                # remove unused node
+                graph[level - 1].pop(idx)
+
+    # remove unused nodes on root level
+    graph[0] = [node for node in graph[0] if len(node.child) != 0]
+
+    for node in graph[0]:
+        ops.update(manhattan(observed, 0, 0, node.row, node.col))
+
+    return ops, graph
+
+
+def trivial(reference, observed):
+    return Variant(0, len(reference), observed)
+
+
+def traverse(reference, observed, graph, atomic=True):
+    def atomic_vars(from_row, to_row, from_col, to_col):
+        n = to_row - from_row + to_col - from_col
+        k = to_col - from_col
+        atomics = []
+        for combo in combinations(range(n), k):
+            c = 0
+            pos = from_row
+            variant = Variant(from_row, from_row)
+            variants = []
+            for i in range(from_row, to_row):
+                variants.append(Variant(i, i + 1))
+            for i in range(k):
+                if combo[i] > c:
+                    pos += combo[i] - c
+                    c = combo[i]
+                    if variant:
+                        variants.append(variant)
+                    variant = Variant(pos, pos, observed[from_col + i])
+                else:
+                    variant.sequence += observed[from_col + i]
+                c += 1
+            if variant:
+                variants.append(variant)
+            atomics.append(variants)
+        return atomics
+
+    def trav(row, col, node, path, paths, subst_range):
+        min_row, min_col, max_row, max_col = subst_range
+
+        if node is None:
+            variant = Variant(row, len(reference), observed[col:])
+            if variant:
+                subst_range[0:4] = [
+                    min(min_row, row),
+                    min(min_col, col),
+                    max(max_row, len(reference)),
+                    max(max_col, len(observed))
+                ]
+
+            if atomic:
+                atomics = atomic_vars(row, len(reference), col, len(observed))
+                for atom in reversed(atomics):
+                    paths.append(path + atom)
+                return len(atomics)
+
+            variants = [var for var in path + [variant] if var]
+            if len(variants) == 0:
+                return 0
+            paths.append(variants)
+            return 1
+
+        variant = Variant(row, node.row - 1, observed[col:node.col - 1])
+        if variant:
+            subst_range[0:4] = [
+                min(min_row, row),
+                min(min_col, col),
+                max(max_row, node.row - 1),
+                max(max_col, node.col - 1)
+            ]
+
+        if atomic:
+            atomics = atomic_vars(row, node.row - 1, col, node.col - 1)
+            count = 0
+            for atom in reversed(atomics):
+                for child in node.child:
+                    count += trav(node.row, node.col, child, path + atom, paths, subst_range)
+            return count
+
+        count = 0
+        for child in node.child:
+            count += trav(node.row, node.col, child, path + [variant], paths, subst_range)
+        return count
+
+    if graph == []:
+        if atomic:
+            return atomic_vars(0, len(reference), 0, len(observed)), trivial(reference, observed)
+        return [[trivial(reference, observed)]], trivial(reference, observed)
+
+    paths = []
+    count = 0
+    subst_range = [len(reference) + 1, len(observed) + 1, 0, 0]
+    for node in graph[0]:
+        count += trav(0, 0, node, [], paths, subst_range)
+
+    if count == 0:
+        return paths, Variant(0, 0)
+
+    min_row, min_col, max_row, max_col = subst_range
+    return paths # , Variant(min_row, max_row, observed[min_col:max_col])
+
+
--- q.py
+++ q.py
@@ -1,8 +1,8 @@
 import sys
 from algebra.lcs.onp import edit as edit_onp
 from algebra.lcs.wupp import edit as edit_test, lcs_graph as graph_test, traversal
-from algebra.lcs.efficient import edit as edit_gold, lcs_graph as graph_gold
-from algebra.variants.variant import to_hgvs
+from algebra.lcs.efficient import edit as edit_gold, build as graph_gold, traverse as traverse_gold
+from algebra.variants.variant import to_hgvs, Variant, patch
 from pprint import pprint
 import random
 
@@ -21,7 +21,7 @@ def compare_matrix(test, gold, f):
 
 def main():
     min_rand = 1
-    max_rand = 42
+    max_rand = 5
 
     if len(sys.argv) == 1:
         reference = "".join(random.choice("ACGT") for _ in range(random.randint(min_rand, max_rand)))
@@ -35,7 +35,7 @@ def main():
     print(reference, observed)
 
     dist_test, matrix_test, nodes_test = edit_test(reference, observed)
-    dist_gold, nodes_gold, matrix_gold = edit_gold(reference, observed)
+    dist_gold, nodes_gold = edit_gold(reference, observed)
     print(dist_test)
     # pprint(matrix_test)
     print(dist_gold)
@@ -43,7 +43,7 @@ def main():
 
     assert dist_test == dist_gold
 
-    compare_matrix(matrix_test, matrix_gold, dist_test)
+    # compare_matrix(matrix_test, matrix_gold, dist_test)
 
     print(nodes_test)
     for level in nodes_test:
@@ -51,10 +51,34 @@ def main():
 
     graph = graph_test(reference, observed, nodes_test)
 
-    # graph_gold(reference, observed, nodes_gold)
+    print("test")
+    paths_test = list(traversal(reference, observed, graph, atomics=True))
+    # s = set()
+    hgvs_test = []
+    for path in paths_test:
+        hgvs_test.append(to_hgvs(path, reference))
+        # s.add(hgvs)
+    for h in sorted(hgvs_test):
+        print(h)
+    # assert len(s) == len(paths)
 
-    for path in traversal(reference, observed, graph):
-        print(to_hgvs(path, reference))
+    # reference = 'ABBC'
+    # v = Variant(1, 2, 'X')
+    # print(v.to_hgvs(reference))
+    # for atomic in v.atomics():
+    #     print(to_hgvs(atomic, reference), patch(reference, atomic))
+
+    print("gold")
+    _, lcs_graph_gold = graph_gold(nodes_gold, reference, observed)
+    paths_gold = traverse_gold(reference, observed, lcs_graph_gold, atomic=False)
+    hgvs_gold = []
+    for path in paths_gold:
+        hgvs_gold.append(to_hgvs(path, reference))
+    for h in sorted(hgvs_gold):
+        print(h)
+
+    assert set(hgvs_test) == set(hgvs_gold)
+
 
 if __name__ == '__main__':
     main()
