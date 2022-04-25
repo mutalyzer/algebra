5f632acebfc47d84ba8692465e2df3380903bb22
jkvis <jkvis@users.noreply.github.com>
2022-04-25 10:20:40 +0200

    Clean up

--- q.py
+++ q.py
@@ -1,10 +1,8 @@
-import sys
-from algebra.lcs.onp import edit as edit_onp
 from algebra.lcs.wupp import edit as edit_test, lcs_graph as graph_test, traversal, to_dot as to_dot_test
 from algebra.lcs.efficient import edit as edit_gold, build as graph_gold, traversal as traversal_gold, to_dot as to_dot_gold
 from algebra.variants.variant import to_hgvs, Variant, patch
-from pprint import pprint
 import random
+import sys
 
 
 def main():
@@ -23,9 +21,7 @@ def main():
     print(reference, observed)
 
     dist_gold, nodes_gold = edit_gold(reference, observed)
-    print(dist_gold)
     dist_test, nodes_test = edit_test(reference, observed)
-    print(dist_test)
 
     assert dist_test == dist_gold
 
@@ -36,7 +32,7 @@ def main():
     hgvs_gold = set()
     for path in paths_gold:
         hgvs_gold.add(to_hgvs(path, reference))
-    print(hgvs_gold)
+    #print(hgvs_gold)
     print(len(hgvs_gold))
 
     print("test")
@@ -46,14 +42,11 @@ def main():
     hgvs_test = set()
     for path in paths_test:
         hgvs_test.add(to_hgvs(path, reference))
-    print(hgvs_test)
+    #print(hgvs_test)
     print(len(hgvs_test))
 
     assert len(list(paths_gold)) == len(list(paths_test))
     assert hgvs_gold == hgvs_test
-
-    print(set(hgvs_test) - set(hgvs_gold))
-    print(set(hgvs_gold) - set(hgvs_test))
 
 
 if __name__ == '__main__':
