61d82f677025046754f1ec03f1ff5541aea9a80c
jkvis <jkvis@users.noreply.github.com>
2022-04-29 22:23:34 +0200

    Fixed asserts

--- q.py
+++ q.py
@@ -1,58 +1,53 @@
-import random
 import sys
-from algebra.lcs.efficient import edit as edit_gold, build as graph_gold, traversal as traversal_gold, to_dot as to_dot_gold
-from algebra.lcs.wupp import edit as edit_test, lcs_graph as graph_test, traversal, to_dot as to_dot_test
-from algebra.variants.variant import to_hgvs, Variant, patch
+from algebra.lcs.efficient import edit as edit_gold, build, traversal as traversal_gold
+from algebra.lcs.wupp import edit as edit_test, lcs_graph, traversal
+from algebra.utils import random_sequence
+from algebra.variants.variant import to_hgvs
 
 
 def main():
-    min_rand = 0
-    max_rand = 4
-
     if len(sys.argv) == 1:
-        reference = "".join(random.choice("ACGT") for _ in range(random.randint(min_rand, max_rand)))
-        observed = "".join(random.choice("ACGT") for _ in range(random.randint(min_rand, max_rand)))
+        reference = random_sequence(21)
+        observed = random_sequence(21)
     elif len(sys.argv) == 3:
         reference = sys.argv[1]
         observed = sys.argv[2]
     else:
         raise("usage")
 
-    print(reference, observed)
+    print(f'"{reference}" "{observed}"')
 
-    # dist_gold, nodes_gold = edit_gold(reference, observed)
+    dist_gold, nodes_gold = edit_gold(reference, observed)
     dist_test, nodes_test = edit_test(reference, observed)
 
-    for idx, level in enumerate(nodes_test):
-        print(idx, level)
+    assert dist_test == dist_gold
 
-    # assert dist_test == dist_gold
+    _, lcs_graph_gold = build(nodes_gold, reference, observed)
+    paths_gold = list(traversal_gold(reference, observed, lcs_graph_gold))
+    hgvs_gold = set()
+    for variants in paths_gold:
+        hgvs_gold.add(to_hgvs(variants, reference))
 
-    # print("gold")
-    # _, lcs_graph_gold = graph_gold(nodes_gold, reference, observed)
-    # print(to_dot_gold(reference, observed, lcs_graph_gold))
-    # paths_gold = traversal_gold(reference, observed, lcs_graph_gold, atomics=True)
-    # hgvs_gold = set()
-    # for path in paths_gold:
-    #     hgvs_gold.add(to_hgvs(path, reference, sort=False))
-    #print(hgvs_gold)
-    # print(len(hgvs_gold))
+    atomic_paths_gold = list(traversal_gold(reference, observed, lcs_graph_gold, atomics=True))
+    atomic_hgvs_gold = set()
+    for variants in atomic_paths_gold:
+        atomic_hgvs_gold.add(to_hgvs(variants, reference))
 
-    # print("test")
-    graph, edges, drops = graph_test(reference, observed, nodes_test)
-    # print(drops)
-    print(to_dot_test(reference, graph))
-    # assert drops <= 25
+    root, _ = lcs_graph(reference, observed, nodes_test)
+    paths_test = list(traversal(root))
+    hgvs_test = set()
+    for variants in paths_test:
+        hgvs_test.add(to_hgvs(variants, reference))
 
-    # paths_test = traversal(graph, atomics=True)
-    # hgvs_test = set()
-    # for path in paths_test:
-    #     hgvs_test.add(to_hgvs(path, reference, sort=False))
-    # #print(hgvs_test)
-    # # print(len(hgvs_test))
-    #
-    # assert len(list(paths_gold)) == len(list(paths_test))
-    # assert hgvs_gold == hgvs_test
+    atomic_paths_test = list(traversal(root, atomics=True))
+    atomic_hgvs_test = set()
+    for variants in atomic_paths_test:
+        atomic_hgvs_test.add(to_hgvs(variants, reference))
+
+    assert len(paths_gold) == len(paths_test)
+    assert len(atomic_paths_gold) == len(atomic_paths_test)
+    assert hgvs_gold == hgvs_test
+    assert atomic_hgvs_gold == atomic_hgvs_test
 
 
 if __name__ == '__main__':
