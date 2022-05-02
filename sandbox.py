66d6a7a40ff23b3c0c51069065da78dde4077c93
jkvis <jkvis@users.noreply.github.com>
2022-05-02 15:09:39 +0200

    Reducing graph for description extractor

--- sandbox.py
+++ sandbox.py
@@ -1,12 +1,57 @@
 import sys
 from algebra.lcs.wupp import edit, lcs_graph, to_dot, traversal
 from algebra.utils import random_sequence, random_variants
+from algebra.variants.parser import Parser
 from algebra.variants.variant import patch, to_hgvs
 
 
+def reduce(reference, root):
+    queue = [(root, None, None)]
+    visited = set()
+    dot = "digraph {\n"
+    while queue:
+        node, parent, in_variant = queue.pop(0)
+        visited.add(node)
+        print("pop", node)
+        if not node.edges:
+            #dot += f'    "{parent.row}_{parent.col}" -> "{node.row}_{node.col}" [label="{to_hgvs(in_variant, reference, sequence_prefix=False)}"];\n'
+            continue
+
+        successors = {}
+        for succ, out_variant in node.edges:
+            if succ in visited:
+                successors = {}
+                continue
+
+            length = len(out_variant[0]) if len(out_variant) else 0
+            if length in successors:
+                if succ in [x for x, _ in successors[length]]:
+                    # repeat
+                    pass
+                else:
+                    successors[length].append((succ, out_variant))
+            else:
+                successors[length] = [(succ, out_variant)]
+
+        for _, edges in sorted(successors.items(), reverse=True):
+            for succ, variant in edges:
+                queue.append((succ, node, variant))
+                print("push", succ)
+
+        if parent and successors:
+            dot += f'    "{parent.row}_{parent.col}" -> "{node.row}_{node.col}" [label="{to_hgvs(in_variant, reference, sequence_prefix=False)}"];\n'
+
+    return dot + "}"
+
+
 def main():
-    reference = random_sequence(10_000, 1_000)
-    variants = list(random_variants(reference, p=0.001, mu_deletion=1.5, mu_insertion=1.7))
+    if len(sys.argv) < 3:
+        reference = random_sequence(100, 100)
+        variants = list(random_variants(reference, p=0.02, mu_deletion=1.5, mu_insertion=1.7))
+    else:
+        reference = sys.argv[1]
+        variants = Parser(sys.argv[2]).hgvs()
+
     observed = patch(reference, variants)
 
     distance, lcs_nodes = edit(reference, observed)
@@ -15,12 +60,10 @@ def main():
     print(distance)
     print(sum([len(level) for level in lcs_nodes]))
 
-    root, edges = lcs_graph(reference, observed, lcs_nodes)
+    root, _ = lcs_graph(reference, observed, lcs_nodes)
 
     print(to_dot(reference, root))
-
-    for description in traversal(root):
-        print(to_hgvs(description, reference, sequence_prefix=False))
+    print(reduce(reference, root))
 
 
 if __name__ == "__main__":
