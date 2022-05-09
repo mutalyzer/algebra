a6141b75649ddae0edef73c9ed00956aad5aa9b9
Mark Santcroos <m.a.santcroos@lumc.nl>
2022-05-09 15:55:42 +0200

    debump version (previous release was 0.x)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="algebra",
-    version="2.0.1",
+    version="1.0.1",
     license="MIT",
     packages=find_packages(),
     extras_require={
