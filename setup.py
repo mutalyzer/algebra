62aff00ad778149abd23aa368f5537391f335a95
Mark Santcroos <m.a.santcroos@lumc.nl>
2022-05-10 15:58:53 +0200

    Read description from markdown.

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import find_packages, setup
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="mutalyzer-algebra",
-    version="1.0.2",
+    version="1.0.3",
     license="MIT",
     author='Jonathan Vis, Mark Santcroos',
     author_email='j.k.vis@lumc.nl, m.a.santcroos@lumc.nl',
@@ -20,4 +24,6 @@ setup(
             "algebra=algebra.__main__:main",
         ],
     },
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
