165c0f64128737199ff9f657a0954134a8456614
Mark Santcroos <m.a.santcroos@lumc.nl>
2022-07-25 16:27:55 +0200

    Parametrize relationship tests.

--- tests/relations/test_supremal_based.py
+++ tests/relations/test_supremal_based.py
@@ -1,10 +1,11 @@
 import pytest
 from algebra import Relation, Variant
 from algebra.lcs import edit, lcs_graph
-from algebra.relations.supremal_based import compare, find_supremal, spanning_variant
+from algebra.relations.supremal_based import (are_disjoint, are_equivalent, compare, contains, find_supremal,
+                                              have_overlap, is_contained, spanning_variant)
 
 
-@pytest.mark.parametrize("reference, lhs, rhs, expected", [
+TESTS = [
     ("AGATCCATTGTCAATGACAT", Variant(7, 11, "T"), Variant(10, 12, "CC"), Relation.OVERLAP),
     ("ATGCTATCCCTCCCCACTCC", Variant(7, 10, "CC"), Variant(10, 15, "TTCCC"), Relation.DISJOINT),
     ("GTGTGTTTTTTTAACAGGGA", Variant(6, 6, "G"), Variant(4, 12, "GGTTTTTTTT"), Relation.DISJOINT),
@@ -12,7 +13,35 @@ from algebra.relations.supremal_based import compare, find_supremal, spanning_va
     ("CCACC", Variant(2, 3, "T"), Variant(3, 3, "T"), Relation.CONTAINS),
     ("CCACC", Variant(2, 2, "T"), Variant(2, 2, "T"), Relation.EQUIVALENT),
     ("CCACC", Variant(1, 1, "T"), Variant(2, 2, "T"), Relation.DISJOINT),
-])
+]
+
+
+@pytest.mark.parametrize("reference, lhs, rhs, expected", TESTS)
+def test_are_equivalent(reference, lhs, rhs, expected):
+    assert are_equivalent(reference, lhs, rhs) == (expected == Relation.EQUIVALENT)
+
+
+@pytest.mark.parametrize("reference, lhs, rhs, expected", TESTS)
+def test_contains(reference, lhs, rhs, expected):
+    assert contains(reference, lhs, rhs) == (expected == Relation.CONTAINS)
+
+
+@pytest.mark.parametrize("reference, lhs, rhs, expected", TESTS)
+def test_is_contained(reference, lhs, rhs, expected):
+    assert is_contained(reference, lhs, rhs) == (expected == Relation.IS_CONTAINED)
+
+
+@pytest.mark.parametrize("reference, lhs, rhs, expected", TESTS)
+def test_have_overlap(reference, lhs, rhs, expected):
+    assert have_overlap(reference, lhs, rhs) == (expected == Relation.OVERLAP)
+
+
+@pytest.mark.parametrize("reference, lhs, rhs, expected", TESTS)
+def test_are_disjoint(reference, lhs, rhs, expected):
+    assert are_disjoint(reference, lhs, rhs) == (expected == Relation.DISJOINT)
+
+
+@pytest.mark.parametrize("reference, lhs, rhs, expected", TESTS)
 def test_compare(reference, lhs, rhs, expected):
     assert compare(reference, lhs, rhs) == expected
 
--- tests/relations/test_variant_based.py
+++ tests/relations/test_variant_based.py
@@ -1,8 +1,35 @@
 import pytest
-from algebra import Relation, Variant, compare
+from algebra import Relation, Variant, are_disjoint, are_equivalent, compare, contains, have_overlap, is_contained
+
+TESTS = [
+]
 
 
-@pytest.mark.parametrize("reference, lhs, rhs, expected", [
-])
+@pytest.mark.parametrize("reference, lhs, rhs, expected", TESTS)
+def test_are_equivalent(reference, lhs, rhs, expected):
+    assert are_equivalent(reference, lhs, rhs) == (expected == Relation.EQUIVALENT)
+
+
+@pytest.mark.parametrize("reference, lhs, rhs, expected", TESTS)
+def test_contains(reference, lhs, rhs, expected):
+    assert contains(reference, lhs, rhs) == (expected == Relation.CONTAINS)
+
+
+@pytest.mark.parametrize("reference, lhs, rhs, expected", TESTS)
+def test_is_contained(reference, lhs, rhs, expected):
+    assert is_contained(reference, lhs, rhs) == (expected == Relation.IS_CONTAINED)
+
+
+@pytest.mark.parametrize("reference, lhs, rhs, expected", TESTS)
+def test_have_overlap(reference, lhs, rhs, expected):
+    assert have_overlap(reference, lhs, rhs) == (expected == Relation.OVERLAP)
+
+
+@pytest.mark.parametrize("reference, lhs, rhs, expected", TESTS)
+def test_are_disjoint(reference, lhs, rhs, expected):
+    assert are_disjoint(reference, lhs, rhs) == (expected == Relation.DISJOINT)
+
+
+@pytest.mark.parametrize("reference, lhs, rhs, expected", TESTS)
 def test_compare(reference, lhs, rhs, expected):
     assert compare(reference, lhs, rhs) == expected
