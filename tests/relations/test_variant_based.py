d37a20142a5d5c0a25083f5c8d7863919c5df720
Mark Santcroos <m.a.santcroos@lumc.nl>
2022-07-26 10:49:11 +0200

    Top-level variant tests.

--- tests/relations/test_variant_based.py
+++ tests/relations/test_variant_based.py
@@ -4,7 +4,12 @@ from algebra import (Relation, Variant, are_disjoint, are_equivalent,
 
 
 TESTS = [
-    ("ACGT", [Variant(0, 1), Variant(1, 2)], [Variant(0, 4)], Relation.IS_CONTAINED),
+    ("ACGT", [Variant(0, 1, ""), Variant(1, 2, "")], [Variant(0, 4, "")], Relation.IS_CONTAINED),
+    ("CC", [Variant(0, 0, "C"), Variant(1, 2, "")], [Variant(0, 0, "C"), Variant(1, 1, "T")], Relation.DISJOINT),
+    ("CTCCG", [Variant(4, 4, "TCC")], [Variant(0, 0, "GT")], Relation.OVERLAP),
+    ("CCCCC", [Variant(4, 5, "")], [Variant(0, 1, "")], Relation.EQUIVALENT),
+    ('AACCCCTTGTTC', [Variant(2, 2, "C")], [Variant(6, 7, "C")], Relation.IS_CONTAINED),
+    ('AACATTTC', [Variant(1, 1, "AC")], [Variant(4, 5, "C")], Relation.OVERLAP),
 ]
 
 
