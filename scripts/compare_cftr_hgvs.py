de535edc63a65f0ab948b08fbb7e68426145c3ba
Mihai Lefter <mihai.lefter@gmail.com>
2022-08-11 16:06:43 +0200

    Further CFTR comparison classification

--- scripts/compare_cftr_hgvs.py
+++ scripts/compare_cftr_hgvs.py
@@ -1,13 +1,57 @@
-import argparse
+from argparse import ArgumentParser
+
+from algebra.utils import fasta_sequence
+from algebra.variants import parse_hgvs
+
+
+def check_repeat_repeat(ncbi, alg, reference):
+    if (
+        "dup" not in ncbi
+        and "del" not in ncbi
+        and "ins" not in ncbi
+        and "dup" not in alg
+        and "del" not in alg
+        and "ins" not in alg
+        and ncbi.count("[") == 1
+        and alg.count("[") == 1
+        and parse_hgvs(ncbi, reference) == parse_hgvs(alg, reference)
+    ):
+        return True
+    else:
+        return False
+
+
+def check_del_repeat(ncbi, alg):
+    if (
+        "del" in ncbi
+        and "ins" not in ncbi
+        and "del" not in alg
+        and "ins" not in alg
+        and "[" in alg
+    ):
+        return True
+    else:
+        return False
+
+
+def check_dup_repeat(ncbi, alg):
+    if "dup" in ncbi and "del" not in alg and "ins" not in alg and "[" in alg:
+        return True
+    else:
+        return False
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Compare the HGVS descriptions of algebra and NCBI")
-    parser.add_argument("--refseq-id", required=True, type=str, help="RefSeq identifier")
+    parser = ArgumentParser(
+        description="Compare the HGVS descriptions of algebra and NCBI"
+    )
     parser.add_argument("--ncbi", required=True, type=str, help="Path to the NCBI file")
     parser.add_argument("--alg", required=True, type=str, help="Path to the algebra file")
-
+    parser.add_argument("--fasta", required=True, type=str, help="Path to the fasta reference file")
     args = parser.parse_args()
+
+    with open(args.fasta, encoding="utf-8") as file:
+        reference = fasta_sequence(file)
 
     descriptions_ncbi = {}
     with open(args.ncbi, encoding="utf-8") as file:
@@ -22,32 +66,36 @@ def main():
                 description_spdi, _, _, _, description_hgvs = line.strip().split(" ")
                 descriptions_alg[description_spdi] = description_hgvs
 
-    # print(len(descriptions_ncbi))
-    # print(len(descriptions_alg))
-    common = 0
-    del_repeat = 0
-    dup_repeat = 0
-    other = 0
+    summary = {
+        "common": {},
+        "del_repeat": {},
+        "dup_repeat": {},
+        "repeat_repeat": {},
+        "other": {},
+    }
+
     for description_spdi in descriptions_ncbi:
         if description_spdi in descriptions_alg:
             ncbi = descriptions_ncbi[description_spdi]
             alg = descriptions_alg[description_spdi]
             if ncbi == alg:
-                common += 1
-            elif "del" in ncbi and "ins" not in ncbi and "del" not in alg and "ins" not in alg and "[" in alg:
-                # print(description_spdi, ncbi, alg)
-                del_repeat += 1
-            elif "dup" in ncbi and "del" not in alg and "ins" not in alg and "[" in alg:
-                # print(description_spdi, ncbi, alg)
-                dup_repeat += 1
+                summary["common"][description_spdi] = (ncbi, alg)
+            elif check_del_repeat(ncbi, alg):
+                summary["del_repeat"][description_spdi] = (ncbi, alg)
+            elif check_dup_repeat(ncbi, alg):
+                summary["dup_repeat"][description_spdi] = (ncbi, alg)
+            elif check_repeat_repeat(ncbi, alg, reference):
+                summary["repeat_repeat"][description_spdi] = (ncbi, alg)
             else:
-                print(description_spdi, ncbi, alg)
-                other += 1
-    print("\n--------\n")
-    print(f"common: {common}")
-    print(f"del_repeat: {del_repeat}")
-    print(f"dup_repeat: {dup_repeat}")
-    print(f"other: {other}")
+                summary["other"][description_spdi] = (ncbi, alg)
+
+    for case in summary:
+        print(f"{case}: {len(summary[case])}")
+    for case in summary:
+        if case is not "common":
+            print(f"\n{case}\n-----\n")
+            for d in summary[case]:
+                print(d, summary[case][d][0], summary[case][d][1])
 
 
 if __name__ == "__main__":
