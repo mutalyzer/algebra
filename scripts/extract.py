bd296a07fc5a9549cefae4f25412cbb3d7633379
jkvis <jkvis@users.noreply.github.com>
2022-08-11 13:44:29 +0200

    Simplify extraction driver

--- scripts/extract.py
+++ scripts/extract.py
@@ -9,9 +9,15 @@ from algebra.variants import parse_spdi, to_hgvs as to_hgvs_simple
 
 def main():
     parser = ArgumentParser(description="Extract variants from line-based input")
-    parser.add_argument("--refseq-id", required=True, type=str, help="RefSeq identifier")
     parser.add_argument("--fasta", required=True, type=str, help="Path to reference fasta file")
     args = parser.parse_args()
+
+    to_skip = [
+        Variant(117642410, 117643194, "AGT"),   # NC
+        Variant(181627, 182411, "AGT"),         # NG
+        Variant(117664686, 117665566, "GGT"),   # NC
+        Variant(203903, 204783, "GGT"),         # NG
+    ]
 
     with open(args.fasta, encoding="utf-8") as file:
         reference = fasta_sequence(file)
@@ -20,12 +26,6 @@ def main():
         spdi = line.strip()
         variant = parse_spdi(spdi)[0]
         print(spdi, variant, end=" ", flush=True)
-        to_skip = [
-            Variant(117642410, 117643194, "AGT"), # NC
-            Variant(181627, 182411, "AGT"), # NG
-            Variant(117664686, 117665566, "GGT"), # NC
-            Variant(203903, 204783, "GGT"), # NG
-        ]
         if variant in to_skip:
             print("skipped")
             continue
