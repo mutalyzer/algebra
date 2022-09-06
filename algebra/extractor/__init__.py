51c2bc43b35586e085af923522054a1f4527d68e
Mark Santcroos <m.a.santcroos@lumc.nl>
2022-09-06 09:15:41 +0200

    Simplify repeat filtering and add tests.

--- algebra/extractor/__init__.py
+++ algebra/extractor/__init__.py
@@ -2,10 +2,12 @@
 
 
 from .extractor import extract, extract_supremal, to_hgvs
+from .repeats import extract_repeats
 
 
 __all__ = [
     "extract",
+    "extract_repeats",
     "extract_supremal",
     "to_hgvs",
 ]
