bba3fa4db8bded26d00db8bfc01b841d8ce06457
Mark Santcroos <m.a.santcroos@lumc.nl>
2022-09-06 14:33:22 +0200

    Snapshot cover work.

--- algebra/extractor/repeats.py
+++ algebra/extractor/repeats.py
@@ -48,6 +48,63 @@ def extract_repeats(word):
     return results
 
 
+def koffer(pmrs, word):
+
+    inv = [[] for _ in range(len(word))]
+
+    for idx, (start, period, count, remainder) in enumerate(pmrs):
+        begin = start + period * 2 - 1
+        end = start + period * count + remainder
+
+        print(idx, begin, end)
+
+        for pos in range(begin, end):
+            inv[pos].append(idx)
+
+    for pos, a in enumerate(inv):
+        print(pos, a)
+
+    max_cover = [0] * len(word)
+
+    for idx in range(len(max_cover)):
+        print("idx", idx)
+        prev = 0
+        if idx > 0:
+            prev = max_cover[idx - 1]
+
+        if not inv[idx]:
+            max_cover[idx] = prev
+
+        else:
+            for pmr in inv[idx]:
+                start, period, count, remainder = pmrs[pmr]
+
+                length = period * ((idx - start + 1) // period)
+                print("length", length)
+
+                prev = 0
+                if idx - length > 0:
+                    prev = max_cover[idx - length]
+
+                    for prev_pmr in inv[idx - length]:
+                        prev_start, prev_period, prev_count, prev_remainder = pmrs[prev_pmr]
+                        pos = prev_start + prev_period * prev_count + prev_remainder - 1
+                        print("pos", pos)
+
+                        length_prime = period * ((idx - pos) // period)
+                        print("length prime", length_prime)
+
+                        if length_prime > 1:
+                            max_cover[idx] = max(max_cover[idx], max_cover[pos] + length_prime)
+
+                max_cover[idx] = max(max_cover[idx], prev + length)
+
+    for pos, a in enumerate(max_cover):
+        print(pos, a)
+
+    return max_cover[-1]
+
+
 def subswithseqs(subs, word):
     for start, period, count, remainder in subs:
         print(str((start, period, count, remainder)) + ",  #", word[start:start + period], word[start:start + period] * count)
@@ -56,13 +113,14 @@ def subswithseqs(subs, word):
 def main():
     if len(sys.argv) < 2:
         return -1
-    input = sys.argv[1]
+    word = sys.argv[1]
 
     # Get all the repeats as (position, period, count, remainder) quadruples
-    subs = extract_repeats(input)
-    print(subs)
-    print(len(subs))
-    subswithseqs(subs, input)
+    runs = extract_repeats(word)
+    print(runs)
+    print(len(runs))
+    subswithseqs(runs, word)
+    koffer(runs, word)
 
 
 if __name__ == '__main__':
