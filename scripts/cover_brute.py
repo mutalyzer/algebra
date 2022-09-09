d7d20f6d5b4a030de67ade39aed0044f866c90a1
Mark Santcroos <m.a.santcroos@lumc.nl>
2022-09-09 14:37:01 +0200

    Iterate driver.

--- scripts/cover_brute.py
+++ scripts/cover_brute.py
@@ -2,21 +2,29 @@ from itertools import chain, combinations
 from os.path import commonprefix
 import sys
 from algebra.utils import random_sequence
+from algebra.extractor.repeats import brutepower, extract_repeats, subswithseqs
+from algebra.extractor.cover import cover
 
 
 def main():
+    counter = 0
     while True:
         word = random_sequence(10, 5)
         pmrs = extract_repeats(word)
+        if not pmrs:
+            continue
+
+        print(word)
         print(pmrs)
         print(len(pmrs))
         subswithseqs(pmrs, word)
 
-        koffer_max = koffer(pmrs, word)
-        brute_max = brutepower(pmrs, word)
+        cover_max = cover(word, pmrs)[-1]
+        brute_max = brutepower(pmrs, word)[0]
 
-        print(word, koffer_max, brute_max)
-        assert koffer_max == brute_max
+        print(counter, word, cover_max, brute_max)
+        assert cover_max == brute_max
+        counter += 1
 
 
 if __name__ == '__main__':
