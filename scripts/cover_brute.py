178b9ab47217ff7c4ac3d7cff3177303de9b2399
Mark Santcroos <m.a.santcroos@lumc.nl>
2022-09-09 17:47:57 +0200

    Alternative brute force.

--- scripts/cover_brute.py
+++ scripts/cover_brute.py
@@ -1,20 +1,33 @@
 from itertools import chain, combinations
 from os.path import commonprefix
+import subprocess
 import sys
 from algebra.utils import random_sequence
 from algebra.extractor.repeats import brutepower, extract_repeats, subswithseqs
 from algebra.extractor.cover import cover
+from algebra.extractor.repeats_alt import _repeats
 
 
 def main():
     counter = 0
     while True:
         word = random_sequence(10, 5)
-        pmrs = extract_repeats(word)
+        print(word)
+        # pmrs1 = extract_repeats(word)
+        pmrs = sorted([(x[1], len(x[0]), x[2], x[3]) for x in _repeats(word)])
+        # print(pmrs1, pmrs)
+
+        #p = subprocess.run(["../repeats/mreps/mreps", "-allowsmall", "-s", word], capture_output=True)
+
+        #pmrs_mreps = eval(p.stdout)
+
+        #assert pmrs == pmrs_mreps
+
         if not pmrs:
             continue
 
-        print(word)
+        # continue
+
         print(pmrs)
         print(len(pmrs))
         subswithseqs(pmrs, word)
