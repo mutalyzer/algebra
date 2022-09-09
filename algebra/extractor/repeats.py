f8224b6d38044e88a459ceae998d8aefff41ece2
Mark Santcroos <m.a.santcroos@lumc.nl>
2022-09-09 14:35:27 +0200

    Remove old implementation of cover.

--- algebra/extractor/repeats.py
+++ algebra/extractor/repeats.py
@@ -49,74 +49,9 @@ def extract_repeats(word):
     return results
 
 
-def koffer(pmrs, word):
-
-    inv = [[] for _ in range(len(word))]
-
-    for idx, (start, period, count, remainder) in enumerate(pmrs):
-        begin = start + period * 2 - 1
-        end = start + period * count + remainder
-
-        print(idx, begin, end)
-
-        for pos in range(begin, end):
-            inv[pos].append(idx)
-
-    for pos, a in enumerate(inv):
-        print(pos, a)
-
-    max_cover = [0] * len(word)
-
-    for idx in range(len(max_cover)):
-        print("idx", idx)
-        prev = 0
-        if idx > 0:
-            prev = max_cover[idx - 1]
-
-        if not inv[idx]:
-            max_cover[idx] = prev
-            continue
-
-        # I / 0 = prev
-        m = prev
-        print("default m + 0", m)
-        for pmr in inv[idx]:
-            start, period, count, remainder = pmrs[pmr]
-            length = period * ((idx - start + 1) // period)
-            print("length", length)
-
-            # II / -1
-            length2 = length - period
-            print("length II", length2)
-            count2 = length2 // period
-            print("count II", count2)
-            idx2 = idx - length2
-            print("index II", idx2)
-            if count2 > 1:
-                m = max(m, max_cover[idx2] + length2)
-            else:
-                m = max(m, max_cover[idx2])
-            print("m after II", m)
-
-            # III / F
-            if idx - length > 0:
-                m = max(m, max_cover[idx - length] + length)
-            else:
-                m = max(m, length)
-
-        print("m", m)
-        max_cover[idx] = m
-
-    for pos, a in enumerate(max_cover):
-        print(pos, a)
-
-    return max_cover[-1]
-
-
 def subswithseqs(subs, word):
     for start, period, count, remainder in subs:
         print(str((start, period, count, remainder)) + ",  #", word[start:start + period], word[start:start + period] * count)
-
 
 
 def powerset(iterable):
@@ -190,7 +125,6 @@ def main():
     print(runs)
     print(len(runs))
     subswithseqs(runs, word)
-    koffer(runs, word)
     _, solutions = brutepower(runs, word)
     for sol in solutions:
         print("Solution")
