28c644da585fb4ea93baec72b5ccf9e290def29b
Mark Santcroos <m.a.santcroos@lumc.nl>
2022-09-13 15:46:02 +0200

    Brute parameter search.

--- scripts/cover_brute.py
+++ scripts/cover_brute.py
@@ -1,30 +1,31 @@
-from algebra.utils import random_sequence
+import itertools
 from algebra.extractor.repeats import brutepower, extract_repeats, subswithseqs
 from algebra.extractor.cover import cover
 
 
 def main():
     counter = 0
-    while True:
-        word = random_sequence(10, 5)
-        print(counter, word)
+    for length in range(11, 12):
+        for word in ["".join(x) for x in itertools.product("AC", repeat=length)]:
 
-        pmrs = extract_repeats(word)
+            print(counter, word)
 
-        if not pmrs:
-            continue
+            pmrs = extract_repeats(word)
 
-        print(pmrs)
-        print(len(pmrs))
-        subswithseqs(pmrs, word)
+            if len(pmrs) < 2:
+                continue
 
-        cover_max = cover(word, pmrs)[-1]
-        brute_max = brutepower(pmrs, word)[0]
+            print(pmrs)
+            print(len(pmrs))
+            subswithseqs(pmrs, word)
 
-        print(counter, word, cover_max, brute_max)
-        assert cover_max == brute_max
+            cover_max = cover(word, pmrs)[-1]
+            brute_max = brutepower(pmrs, word)[0]
 
-        counter += 1
+            print(counter, word, cover_max, brute_max)
+            assert cover_max == brute_max
+
+            counter += 1
 
 
 if __name__ == '__main__':
