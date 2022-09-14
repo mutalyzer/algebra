8ad0b2a8512ac9a26ba96282f8b0af679bc311a2
Mark Santcroos <m.a.santcroos@lumc.nl>
2022-09-14 10:51:04 +0200

    Become less enthousiastic.

--- algebra/extractor/__init__.py
+++ algebra/extractor/__init__.py
@@ -2,12 +2,10 @@
 
 
 from .extractor import extract, extract_supremal, to_hgvs
-from .repeats import extract_repeats
 
 
 __all__ = [
     "extract",
-    "extract_repeats",
     "extract_supremal",
     "to_hgvs",
 ]
