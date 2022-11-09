635e232c843df0d14b03be34db1bf1f7fea7e029
mlefter <m.lefter@lumc.nl>
2022-11-09 16:29:48 +0100

    WIP

--- algebra/extractor/repeats_to_hgvs.py
+++ algebra/extractor/repeats_to_hgvs.py
@@ -1,4 +1,8 @@
 from copy import deepcopy
+import sys
+from algebra.extractor.cover import find_pmrs, overlapping, inv_array, print_tables, brute_cover, cover
+
+
 
 
 def to_hgvs(word, max_cover, inv, pmrs, overlap):
@@ -62,3 +66,56 @@ def to_hgvs(word, max_cover, inv, pmrs, overlap):
             return paths
         else:
             nodes.pop(current_node)
+
+
+def to_hgvs_2(word, max_cover, inv, pmrs, overlap):
+    def repeats(idx, sol=[], indent=""):
+        print(indent, "idx:", idx, sol)
+        print(indent, "----------")
+        if idx == -1:
+            yield sol
+        else:
+            for p in inv[idx]:
+                print(indent, "pattern:", p, pmrs[p])
+                if overlap[p] > 0:
+                    end = overlap[p] + 1
+                else:
+                    end = pmrs[p][0] + 1
+                for i in range(pmrs[p][0], end):
+                    print(indent, "i:", i, "; idx:", idx, "; idx - i + 1:", idx - i + 1)
+                    if (idx - i + 1) % pmrs[p][1] == 0:
+                        if i == 0 or (i > 0 and idx - i + 1 + max_cover[i-1] == max_cover[idx]):
+                            print(indent, "extend with:", (p, i, idx))
+                            sol.append((p, i, idx))
+                            yield from repeats(i-1, sol, indent + "  ")
+                            sol.pop(-1)
+                            print(indent, "back to idx:", idx, sol)
+
+    print("\n\n====\n")
+    for r in repeats(len(word)-1, []):
+        print("\n==>", r, "\n")
+
+
+def main():
+    if len(sys.argv) < 2:
+        print(f"usage: {sys.argv[0]} word", file=sys.stderr)
+        return
+
+    word = sys.argv[1]
+    n = len(word)
+    print(n, word)
+
+    pmrs = sorted(find_pmrs(word))
+    overlap = overlapping(pmrs)
+    for idx, pmr in enumerate(pmrs):
+        print(f"{pmr},  # {idx:2}: {word[pmr[0]:pmr[0] + pmr[1]]} : {overlap[idx]}")
+
+    inv = inv_array(n, pmrs)
+    max_cover = cover(word, pmrs)
+    print_tables(n, word, inv, max_cover)
+    print(brute_cover(word, pmrs))
+    to_hgvs_2(word, max_cover, inv, pmrs, overlap)
+
+
+if __name__ == '__main__':
+    main()
