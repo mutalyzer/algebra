ff48350564b52e956d5ac3a5a9d5c030a403f074
Mark Santcroos <m.a.santcroos@lumc.nl>
2022-11-29 22:55:17 +0100

    Use walk directly.

--- scripts/cover_brute.py
+++ scripts/cover_brute.py
@@ -1,11 +1,11 @@
 import itertools
 from algebra.extractor.cover import cover, brute_cover_alt, cover_length, find_pmrs, overlapping, inv_array, cartesian_cover
-from algebra.extractor.walker import path2hgvs, inv2paths, unique_pmrs
+from algebra.extractor.walker import path2hgvs, unique_pmrs, walk
 
 
 def main():
     counter = 0
-    for length in range(21, 22):
+    for length in range(2, 21):
         for word in ["".join(x) for x in itertools.product("AC", repeat=length)]:
 
             print(counter, word)
@@ -31,16 +31,11 @@ def main():
             inv = inv_array(n, pmrs)
 
             max_cover = cover(word, pmrs)
-            paths = list(inv2paths(inv, pmrs, max_cover, overlap, word))
-            # print(paths)
+            paths = list(walk(inv, pmrs, overlap, word, len(max_cover) - 1, []))
             all_max = max(map(cover_length, paths))
-            # print(all_max)
             paths_uniq = [path for path in paths if unique_pmrs(path)]
             uniq_max = max(map(cover_length, paths_uniq))
-            # print(paths_uniq)
-            inv_results_all = sorted([path2hgvs(path, word) for path in paths if cover_length(path) == all_max])
             inv_results_uniq = sorted([path2hgvs(path, word) for path in paths if unique_pmrs(path) and cover_length(path) == uniq_max])
-            # print(inv_results_all)
             print(inv_results_uniq)
 
             assert brute_results == inv_results_uniq
