2da6d92e3c4e109c6bd8196e6d67387b87f9f515
Mihai <M.Lefter@lumc.nl>
2023-02-11 12:53:37 +0100

    WIP

---
+++ algebra/lcs/all_lcs_mdfa.py
@@ -0,0 +1,305 @@
+from collections import deque
+from copy import deepcopy
+
+from algebra.variants import Variant
+from algebra.variants.variant import to_hgvs
+
+
+class _Node:
+    """Node class for (stretches of) matching symbols.
+    FOR INTERNAL USE ONLY.
+    """
+
+    def __init__(self, row, col, length=0):
+        self.row = row
+        self.col = col
+        self.length = length
+        self.max_length = length
+
+        self.edges = []
+        self.pre_edges = []
+        self.incoming = 0
+
+    def __eq__(self, other):
+        return (
+            self.row == other.row
+            and self.col == other.col
+            and self.length == other.length
+        )
+
+    def __hash__(self):
+        return hash((self.row, self.col, self.length, self.max_length))
+
+    def __repr__(self):
+        return f"{self.row, self.col, self.length}"
+
+
+def edit(reference, observed):
+    """Calculate the simple edit distance between two strings and
+    construct a collection of LCS nodes.
+
+    Returns
+    -------
+    int
+        The simple edit distance.
+    list
+        A collection of LCS nodes in order to construct the LCS graph.
+
+    See Also
+    --------
+    `lcs_graph` : Constructs the LCS graph from LCS nodes.
+    """
+
+    def expand(idx):
+        nonlocal max_lcs_pos
+        start = diagonals[offset + idx]
+        if idx > 0:
+            row = start
+            col = row + idx
+            end = max(diagonals[offset + idx - 1] - 1, diagonals[offset + idx + 1])
+        elif idx < 0:
+            col = start
+            row = col - idx
+            end = max(diagonals[offset + idx - 1], diagonals[offset + idx + 1] - 1)
+        else:
+            row = start
+            col = row + idx
+            end = max(diagonals[offset + idx - 1], diagonals[offset + idx + 1])
+
+        matching = False
+        match_row = 0
+        match_col = 0
+        for _ in range(start, end):
+            if reference[row] == observed[col]:
+                if not matching:
+                    match_row = row
+                    match_col = col
+                matching = True
+            elif matching:
+                lcs_pos = (
+                    (row + col)
+                    - (
+                        abs(delta)
+                        + 2 * it
+                        - abs((len(reference) - row) - (len(observed) - col))
+                    )
+                ) // 2 - 1
+                max_lcs_pos = max(lcs_pos, max_lcs_pos)
+                lcs_nodes[lcs_pos].append(
+                    _Node(match_row + 1, match_col + 1, row - match_row)
+                )
+                matching = False
+            row += 1
+            col += 1
+
+        steps = end + 1
+        if not matching:
+            match_row = row
+            match_col = col
+        while (
+            row < len(reference)
+            and col < len(observed)
+            and reference[row] == observed[col]
+        ):
+            matching = True
+            row += 1
+            col += 1
+            steps += 1
+        if matching:
+            lcs_pos = (
+                (row + col)
+                - (
+                    abs(delta)
+                    + 2 * it
+                    - abs((len(reference) - row) - (len(observed) - col))
+                )
+            ) // 2 - 1
+            max_lcs_pos = max(lcs_pos, max_lcs_pos)
+            lcs_nodes[lcs_pos].append(
+                _Node(match_row + 1, match_col + 1, row - match_row)
+            )
+
+        return steps
+
+    lcs_nodes = [[] for _ in range(min(len(reference), len(observed)))]
+    max_lcs_pos = 0
+
+    delta = len(observed) - len(reference)
+    offset = len(reference) + 1
+    diagonals = [0] * (len(reference) + len(observed) + 3)
+    it = 0
+
+    if delta >= 0:
+        lower = 0
+        upper = delta
+    else:
+        lower = delta
+        upper = 0
+
+    while diagonals[offset + delta] <= max(len(reference), len(observed)) - abs(delta):
+        for idx in range(lower - it, delta):
+            diagonals[offset + idx] = expand(idx)
+
+        for idx in range(upper + it, delta, -1):
+            diagonals[offset + idx] = expand(idx)
+
+        diagonals[offset + delta] = expand(delta)
+        it += 1
+
+    return abs(delta) + 2 * (it - 1), lcs_nodes[: max_lcs_pos + 1]
+
+
+def to_dot(reference, source, sink):
+    """The LCS graph in Graphviz DOT format."""
+
+    def traverse():
+        # breadth-first traversal
+        visited = {source}
+        queue = deque([source])
+        while queue:
+            node = queue.popleft()
+            for succ, variant in node.edges:
+                if variant:
+                    yield (
+                        f'"{node.row}_{node.col}_{node.max_length}" -> "{succ.row}_{succ.col}_{succ.max_length}"'
+                        f' [label="{to_hgvs(variant, reference)}"];'
+                    )
+                if succ not in visited:
+                    visited.add(succ)
+                    queue.append(succ)
+
+    return (
+        "digraph {\n"
+        "    rankdir=LR\n"
+        "    edge[fontname=monospace]\n"
+        "    node[shape=circle]\n"
+        "    si[shape=point]\n"
+        "    " + f'"{sink.row}_{sink.col}_{sink.max_length}"' + "[shape=doublecircle]\n"
+        "    si->" + f'"{source.row}_{source.col}_{source.max_length}"' + "\n"
+        "    " + "\n    ".join(traverse()) + "\n}"
+    )
+
+
+def print_lcs_nodes(lcs_nodes):
+    print("\n=========")
+    print("lcs nodes")
+    print("=========")
+    for idx, level in enumerate(lcs_nodes):
+        print(f"level: {idx}")
+        print("--------")
+        for node in level:
+            print(node, node.edges)
+        print("--------")
+    print("=========\n")
+
+
+def get_variant_offset(pred, succ, observed):
+    start = pred.row + pred.length - 1
+    end = succ.row + succ.length - 2
+    seq_start = pred.col + pred.length - 1
+    seq_end = succ.col + succ.length - 2
+    return Variant(start, end, observed[seq_start:seq_end])
+
+
+def variant_possible_offset(pred, succ):
+    return (
+        pred.row + pred.length - 1 < succ.row + succ.length - 1
+        and pred.col + pred.length - 1 < succ.col + succ.length - 1
+    )
+
+
+def variant_possible(pred, succ):
+    return pred.row < succ.row and pred.col < succ.col
+
+
+def should_merge_sink(lcs_nodes, sink):
+    return (
+        lcs_nodes[-1][-1].row + 1 == sink.row and lcs_nodes[-1][-1].col + 1 == sink.col
+    )
+
+
+def get_node_with_length(node):
+    return node.row + node.length - 1, node.col + node.length - 1
+
+
+def lcs_graph_mdfa(reference, observed, lcs_nodes):
+    sink = _Node(len(reference) + 1, len(observed) + 1, 1)
+    source = _Node(0, 0, 1)
+
+    predecessors = lcs_nodes[-1]
+
+    if should_merge_sink(lcs_nodes, sink):
+        lcs_nodes[-1][-1].length += 1
+        sink = lcs_nodes[-1][-1]
+        predecessors = lcs_nodes[-1][:-1]
+
+    for idx, level in enumerate(lcs_nodes):
+        for node in level:
+            node.max_length = node.length
+
+    successors = [sink]
+
+    lcs_pos = len(lcs_nodes) - 1
+
+    while predecessors:
+        print(f"\nfor level {lcs_pos} to {lcs_pos + 1}")
+        print("----------------")
+        print(predecessors)
+        print(successors)
+        print("----------------")
+
+        while successors:
+            node = successors.pop(0)
+            print(f"\n- working with node {node} as {get_node_with_length(node)}")
+
+            if not node.edges and node != sink:
+                print(f"- skipped {node}")
+                continue
+
+            for i, pred in enumerate(predecessors):
+                print(f" - predecessor node {pred} as {get_node_with_length(pred)}")
+                if variant_possible_offset(pred, node):
+                    variant = get_variant_offset(pred, node, observed)
+
+                    if pred.pre_edges and pred.incoming == lcs_pos:
+                        print(f"\n  - inversion")
+                        upper_node = deepcopy(pred)
+                        upper_node.max_length = pred.length
+                        upper_node.edges.append((node, [variant]))
+                        predecessors[i] = upper_node
+                        predecessor = upper_node
+                        print(f"   - split pred {pred}, pred.incoming = {pred.incoming}")
+                        print(f"   - upper_node: {upper_node}; upper_node max_length {upper_node.max_length} upper_node edges: {upper_node.edges}\n")
+                    else:
+                        predecessor = pred
+                        predecessor.edges.append((node, [variant]))
+                        node.pre_edges.append((predecessor, [variant]))
+                    node.incoming = lcs_pos
+                    print(f"  - added edge ({node} [{variant}]),  to {predecessor}, predecessor max_length {predecessor.max_length}")
+                    print(f"  - predecessor edges: {[(e[0], e[0].max_length) for e in predecessor.edges]}")
+                    print(f"  - set incoming for {node} to {node.incoming}")
+
+            print(" - check if current node should be added to the predecessors")
+            if node.length > 1:
+                node.length -= 1
+                predecessors = sorted(
+                    predecessors + [node], key=lambda n: (n.row, n.col)
+                )
+                print(f"  - added {node} in the predecessors {predecessors}")
+
+        lcs_pos -= 1
+        successors = predecessors
+
+        if lcs_pos > -1:
+            predecessors = lcs_nodes[lcs_pos]
+        elif lcs_pos == -1:
+            if successors[0].row == 1 and successors[0].col == 1:
+                source = successors[0]
+                source.row = 0
+                source.col = 0
+                successors.pop(0)
+            predecessors = [source]
+        else:
+            break
+
+    return source, sink
