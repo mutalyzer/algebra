250b2e15bfd1b06d0879b143e30e2da9681b027a
jkvis <jkvis@users.noreply.github.com>
2023-08-01 11:59:40 +0200

    Default exports

--- algebra/lcs/__init__.py
+++ algebra/lcs/__init__.py
@@ -1,13 +1,12 @@
 """Longest Common Subsequence alignments."""
 
 
-from .all_lcs import edit, lcs_graph, traversal
-from .distance_only import edit as edit_distance_only
+from .all_lcs import lcs_graph, traversal
+from .distance_only import edit
 
 
 __all__ = [
     "edit",
-    "edit_distance_only",
     "lcs_graph",
     "traversal",
 ]
