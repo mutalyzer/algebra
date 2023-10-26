2e3ebae6a9ae54dcc64c54bb92a5758a3bd3a4bb
jkvis <jkvis@users.noreply.github.com>
2023-10-26 11:59:28 +0200

    Refactor supremal module

---
+++ algebra/lcs/supremals.py
@@ -0,0 +1,80 @@
+"""Functions to find supremal variants."""
+
+
+from operator import attrgetter
+from os.path import commonprefix
+from ..lcs.all_lcs import build_graph, edit
+from ..variants import Variant, patch
+
+
+def trim(lhs, rhs):
+    """Find the lengths of the common prefix and common suffix between
+    two sequences."""
+    idx = len(commonprefix([lhs, rhs]))
+    return idx, len(commonprefix([lhs[idx:][::-1], rhs[idx:][::-1]]))
+
+
+def supremal(reference, variants, offset=10):
+    """Iteratively find the supremal variant for an allele by repeatedly
+    widening a range of influence.
+
+    Parameters
+    ----------
+    reference : str
+        The reference sequence.
+    variants : list of `Variant`s
+        The allele of interest.
+
+    Other Parameters
+    ----------------
+    offset : int, optional
+        The minimum offset around the variant.
+
+    Returns
+    -------
+    supremal : `Variant`
+        The supremal variant.
+    root : `_Node`
+        The LCS graph in which the supremal was determined.
+    observed : str
+        The observed sequence for the given LCS graph.
+    shift : int
+        The offset by which the variants in the LCS graph are shifted.
+    """
+
+    def spanning_variant(reference, observed, variants, shift=0):
+        if not variants:
+            return Variant(0, 0, "")
+
+        start = min(variants, key=attrgetter("start")).start
+        end = max(variants, key=attrgetter("end")).end
+        return Variant(start, end, observed[shift + start:shift + len(observed) - (len(reference) - end)])
+
+    variant = spanning_variant(reference, patch(reference, variants), variants)
+
+    offset = max(offset, len(variant) // 2, 1)
+
+    while True:
+        start = max(0, variant.start - offset)
+        end = min(len(reference), variant.end + offset)
+
+        observed = reference[start:variant.start] + variant.sequence + reference[variant.end:end]
+
+        _, lcs_nodes = edit(reference[start:end], observed)
+        root, edges = build_graph(reference[start:end], observed, lcs_nodes, start)
+        variant = spanning_variant(reference[start:end], observed, edges, -start)
+
+        if not variant:
+            return variant, root, observed, start
+
+        if ((variant.start > start or variant.start == 0) and
+                (variant.end < end or variant.end == len(reference))):
+            return variant, root, observed, start
+
+        offset *= 2
+
+
+def supremal_sequence(reference, observed):
+    """The supremal variant for two sequences."""
+    prefix_len, suffix_len = trim(reference, observed)
+    return supremal(reference, [Variant(prefix_len, len(reference) - suffix_len, observed[prefix_len:len(observed) - suffix_len])])
--- algebra/relations/supremal_based.py
+++ algebra/relations/supremal_based.py
@@ -1,14 +1,11 @@
-"""Functions to find, compare and construct supremal variants."""
+"""Functions to compare supremal variants."""
 
 
-from operator import attrgetter
 from .relation import Relation
 from .sequence_based import (are_disjoint as sequence_based_are_disjoint,
                              compare as sequence_based_compare,
                              contains as sequence_based_contains,
                              have_overlap as sequence_based_have_overlap)
-from ..lcs.all_lcs import build_graph, edit
-from ..variants import Variant
 
 
 def are_equivalent(_reference, lhs, rhs):
@@ -80,76 +77,3 @@ def compare(reference, lhs, rhs):
     lhs_observed = reference[min(start, lhs.start):lhs.start] + lhs.sequence + reference[lhs.end:max(end, lhs.end)]
     rhs_observed = reference[min(start, rhs.start):rhs.start] + rhs.sequence + reference[rhs.end:max(end, rhs.end)]
     return sequence_based_compare(reference[start:end], lhs_observed, rhs_observed)
-
-
-def spanning_variant(reference, observed, variants, shift=0):
-    """Calculate the minimum spanning variant for a collection of
-    variants. If the collection of variants is the collection of all
-    minimal variants the minimum spanning variant is the supremal
-    variant.
-
-    See Also
-    --------
-    `algebra.lcs.all_lcs.build_graph` : The collection (edges) of all
-    minimal variants.
-    """
-
-    if not variants:
-        return Variant(0, 0, "")
-
-    start = min(variants, key=attrgetter("start")).start
-    end = max(variants, key=attrgetter("end")).end
-    return Variant(start, end, observed[shift + start:shift + len(observed) - (len(reference) - end)])
-
-
-def find_supremal(reference, variant, offset=10):
-    """Iteratively find the supremal variant by repeatedly widening a
-    range of influence.
-
-    Parameters
-    ----------
-    reference : str
-        The reference sequence.
-    variant : `Variant`
-        The variant of interest. Allele descriptions should be converted
-        to a single (delins type) variant.
-
-    Other Parameters
-    ----------------
-    offset : int, optional
-        The minimum offset around the variant.
-
-    Returns
-    -------
-    supremal : `Variant`
-        The supremal variant.
-    root : `_Node`
-        The root of the LCS-graph in which the supremal was determined.
-    observed : str
-        The observed sequence for the given LCS-graph.
-
-    See Also
-    --------
-    `spanning_variant` : The minimum spanning (delins) variant for an allele.
-    """
-
-    offset = max(offset, len(variant) // 2, 1)
-
-    while True:
-        start = max(0, variant.start - offset)
-        end = min(len(reference), variant.end + offset)
-
-        observed = reference[start:variant.start] + variant.sequence + reference[variant.end:end]
-
-        _, lcs_nodes = edit(reference[start:end], observed)
-        root, edges = build_graph(reference[start:end], observed, lcs_nodes, start)
-        supremum = spanning_variant(reference[start:end], observed, edges, -start)
-
-        if not supremum:
-            return supremum, root, observed
-
-        if ((supremum.start > start or supremum.start == 0) and
-                (supremum.end < end or supremum.end == len(reference))):
-            return supremum, root, observed
-
-        offset *= 2
