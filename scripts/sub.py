bd1e9f0643dfa6bea3cb5b05c17b6953d26ca23f
jkvis <jkvis@users.noreply.github.com>
2023-12-18 16:27:59 +0100

    WIP: correct variants?

--- scripts/sub.py
+++ scripts/sub.py
@@ -1,43 +1,34 @@
 from itertools import combinations
 from sys import argv
 from algebra.lcs import bfs_traversal, supremal
+from algebra.utils import to_dot
 from algebra.variants import Variant, parse_hgvs
 
 
 def unique_matches(graph):
-    results = []
     for node in {sink for _, sink, _ in bfs_traversal(graph)} | {graph}:
-        for i in range(node.length):
-            results.append((node.row + i, node.col + i))
         if node.length == 0:
-            assert node == graph or not node.edges
-            results.append((node.row, node.col))
-
-    return sorted(results)
-
-
-def dominates(lhs, rhs):
-    return lhs[0] > rhs[0] and lhs[1] > rhs[1]
-
-
-def delins(observed, lhs, rhs):
-    return Variant(lhs[0] + 1, rhs[0], observed[lhs[1]:rhs[1] - 1])
+            if node == graph:
+                yield node.row - 1, node.col - 1
+            elif not node.edges:
+                yield node.row, node.col + 1
+            else:
+                raise ValueError
+        for i in range(node.length):
+            yield node.row + i, node.col + i
 
 
 def main():
     reference = argv[1]
     variants = parse_hgvs(argv[2], reference)
+    supremal_variant, graph = supremal(reference, variants)
+    print("\n".join(to_dot(reference, graph)))
 
-    sup, graph = supremal(reference, variants)
-
-    for lhs, rhs in combinations(unique_matches(graph), 2):
-
-        if dominates(lhs, rhs):
-            lhs, rhs = rhs, lhs
-
-        if dominates(rhs, lhs):
-            variant = delins(sup.sequence, lhs, rhs)
+    shift = graph.row
+    for lhs, rhs in combinations(sorted(unique_matches(graph)), 2):
+        if rhs[0] > lhs[0] and rhs[1] > lhs[1]:
             print(lhs, rhs)
+            variant = Variant(lhs[0] + 1, rhs[0], supremal_variant.sequence[lhs[1] + 1 - shift:rhs[1] - shift])
             print(variant.to_hgvs(reference))
 
 
