b2742a4badea7d6f7fce5124acf2d158fe4f61fb
jkvis <jkvis@users.noreply.github.com>
2024-01-20 15:51:36 +0100

    Font-family for consistent typesetting

--- algebra/utils.py
+++ algebra/utils.py
@@ -26,7 +26,8 @@ def to_dot(reference, graph, labels=True, hgvs=True, atomics=False):
 
     yield "digraph{"
     yield "rankdir=LR"
-    yield f"node[fixedsize=true,shape=circle,width={.7 if labels else 1}]"
+    yield 'edge[fontname="monospace"]'
+    yield f'node[fixedsize=true,fontname="serif",shape=circle,width={".8" if labels else "1"}]'
     yield "si[shape=point,width=.1]"
     yield "si->s0"
 
