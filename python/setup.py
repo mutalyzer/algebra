88fd99d343cadf49c73899fac50d223ef0a56742
jkvis <jkvis@users.noreply.github.com>
2024-03-21 11:02:20 +0100

    Compile args

--- python/setup.py
+++ python/setup.py
@@ -1,17 +1,23 @@
 from setuptools import Extension, setup
 
 
+compile_args = [
+    "-Wextra",
+    "-Wpedantic",
+    "-std=c99",
+]
+
+
 setup(
     ext_modules = [
-        Extension("algebra_ext",
+        Extension("algebra_ext", extra_compile_args=compile_args,
                   sources=[
                     "ext/algebra_ext.c"
-                  ],
-                 ),
-        Extension("lcs_ext",
+                  ]),
+        Extension("lcs_ext", extra_compile_args=compile_args,
                   sources=[
-                    "ext/lcs_ext.c", "src/edit.c"
-                  ],
-                 ),
+                    "ext/lcs_ext.c",
+                    "src/edit.c"
+                  ]),
     ],
 )
