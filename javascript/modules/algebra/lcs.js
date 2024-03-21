9bff2c4c01763c61facd33548a413107af3dcc7b
jkvis <jkvis@users.noreply.github.com>
2024-03-21 19:16:57 +0100

    Simplify single symbol export

--- javascript/modules/algebra/lcs.js
+++ javascript/modules/algebra/lcs.js
@@ -1,12 +1,9 @@
 import { wasmInstance } from "../wasm.js";
 
 
-let edit;
+export let edit;
 
 
 await wasmInstance("./algebra.wasm").then(wasm => {
     edit = (lhs, rhs) => wasm.exports.edit(lhs, rhs ?? 42);
 });
-
-
-export { edit };
