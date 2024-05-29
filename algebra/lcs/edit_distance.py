8bf0348bd98c6f0998c1aaf023b3b2fbc20acff0
jkvis <jkvis@users.noreply.github.com>
2024-05-29 15:14:29 +0200

    Refactored Wu's algorithm

--- algebra/lcs/edit_distance.py
+++ algebra/lcs/edit_distance.py
@@ -20,13 +20,13 @@ pp. 317-323.
 
 def edit_distance(a, b):
     """Calculate the simple edit distance between two strings."""
-    def snake(m, n, k, pp, ppp):
-        y = max(pp, ppp)
-        x = y - k
-        while x < m and y < n and a[x] == b[y]:
-            x += 1
-            y += 1
-        return y
+    def snake(k, lower, upper):
+        col = max(lower, upper)
+        row = col - k
+        while row < m and col < n and a[row] == b[col]:
+            row += 1
+            col += 1
+        return col
 
     m = len(a)
     n = len(b)
@@ -42,10 +42,10 @@ def edit_distance(a, b):
     while True:
         p += 1
         for k in range(-p, delta):
-            fp[k + offset] = snake(m, n, k, fp[k - 1 + offset] + 1, fp[k + 1 + offset])
+            fp[k + offset] = snake(k, fp[k - 1 + offset] + 1, fp[k + 1 + offset])
         for k in range(delta + p, delta, -1):
-            fp[k + offset] = snake(m, n, k, fp[k - 1 + offset] + 1, fp[k + 1 + offset])
-        fp[delta + offset] = snake(m, n, delta, fp[delta - 1 + offset] + 1, fp[delta + 1 + offset])
+            fp[k + offset] = snake(k, fp[k - 1 + offset] + 1, fp[k + 1 + offset])
+        fp[delta + offset] = snake(delta, fp[delta - 1 + offset] + 1, fp[delta + 1 + offset])
 
         if fp[delta + offset] >= n:
             break
--- tests/lcs/test_edit_distance.py
+++ tests/lcs/test_edit_distance.py
@@ -4,6 +4,8 @@ from algebra.lcs import edit_distance
 
 @pytest.mark.parametrize("reference, observed, expected", [
     ("CTCGGCATTA", "GGCTGGCTGT", 6),
+    ("TTT", "TTTTAT", 3),
+    ("TTTTAT", "TTT", 3),
 ])
 def test_edit_distance(reference, observed, expected):
     assert edit_distance(reference, observed) == expected
