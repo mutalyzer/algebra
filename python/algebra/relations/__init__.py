d3b045ac18ac2923c26c568a579594f9be7fe69c
jkvis <jkvis@users.noreply.github.com>
2025-08-08 14:33:22 +0200

    WIP: pytests for compare

--- python/algebra/relations/__init__.py
+++ python/algebra/relations/__init__.py
@@ -1,3 +1,15 @@
+"""Calculate the relation between two variants using the definitions
+from [1]_. Variants are given as alleles, i.e., a list of variants
+applied to some reference sequence. Both variants are subject to the same
+reference sequence.
+
+References
+----------
+[1] J.K. Vis, M.A. Santcroos, W.A. Kosters and J.F.J. Laros.
+"A Boolean Algebra for Genetic Variants." In: Bioinformatics (2023).
+"""
+
+
 from algebra_ext import Relation, compare
 
 
--- python/tests/test_compare.py
+++ python/tests/test_compare.py
@@ -2,8 +2,15 @@ import pytest
 from algebra import LCSgraph, Relation, Variant, compare
 
 
-def test_compare():
-    reference = "AAAAA"
-    lhs = LCSgraph.from_variants(reference, [Variant(0, 0, "T")])
-    rhs = LCSgraph.from_variants(reference, [Variant(0, 0, "TA")])
-    assert compare(reference, lhs, rhs) == Relation.is_contained
+@pytest.mark.parametrize("reference, lhs, rhs, expected", [
+    ("CC", [Variant(0, 0, "C"), Variant(1, 2, "")], [Variant(0, 0, "C"), Variant(1, 1, "T")], Relation.disjoint),
+    ("CTCCG", [Variant(4, 4, "TCC")], [Variant(0, 0, "GT")], Relation.overlap),
+    ("CCCCC", [Variant(4, 5, "")], [Variant(0, 1, "")], Relation.equivalent),
+    ("AACCCCTTGTTC", [Variant(2, 2, "C")], [Variant(6, 7, "C")], Relation.is_contained),
+    ("AACATTTC", [Variant(1, 1, "AC")], [Variant(4, 5, "C")], Relation.overlap),
+    ("AAA", [Variant(1, 1, "T")], [Variant(1, 1, "C")], Relation.disjoint),
+])
+def test_compare(reference, lhs, rhs, expected):
+    lhs_graph = LCSgraph.from_variants(reference, lhs)
+    rhs_graph = LCSgraph.from_variants(reference, rhs)
+    assert compare(reference, lhs_graph, rhs_graph) == expected
