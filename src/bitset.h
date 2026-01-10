c87575ad4c2fb4f0e478132241eeef1ace139dcc
jkvis <jkvis@users.noreply.github.com>
2026-01-10 11:00:02 +0100

    More bitsets

--- src/bitset.h
+++ src/bitset.h
@@ -2,6 +2,7 @@
 #define GVA_BITSET_H
 
 
+#include <stdbool.h>    // bool
 #include <stddef.h>     // size_t
 
 #include "../include/allocator.h"   // GVA_Allocator
@@ -17,6 +18,18 @@ bitset_add(size_t self[static 1], size_t const start, size_t const end);
 
 size_t
 bitset_intersection_cnt(size_t const lhs[static 1], size_t const rhs[static 1]);
+
+
+void
+bitset_set(size_t self[static 1], size_t const value);
+
+
+void
+bitset_clear(size_t self[static 1], size_t const value);
+
+
+bool
+bitset_test(size_t const self[static 1], size_t const value);
 
 
 size_t*
