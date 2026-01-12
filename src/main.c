8ff2415dc42192c1c5bbcacece8ecf4153229117
Mark Santcroos <m.a.santcroos@lumc.nl>
2026-01-12 12:24:55 +0100

    Make blob helpers.

--- src/main.c
+++ src/main.c
@@ -1362,6 +1362,87 @@ extract_main(int argc, char* argv[static argc + 1]) {
 } // extract_main
 
 
+int make_ref_blob_main(int argc, char* argv[static argc + 1]) {
+    if (argc < 2) {
+        fprintf(stderr, "usage: %s reference\n", argv[0]);
+        return EXIT_FAILURE;
+    } // if
+
+    errno = 0;
+    FILE *stream = fopen(argv[1], "r");
+    if (stream == NULL) {
+        fprintf(stderr, "error: %s\n", strerror(errno));
+        return EXIT_FAILURE;
+    } // if
+
+    GVA_String reference = gva_fasta_sequence(gva_std_allocator, stream);
+    fclose(stream);
+    fprintf(stderr, "reference length: %zu\n", reference.len);
+
+    fasta_blob_write(stdout, reference);
+
+    return EXIT_SUCCESS;
+} // make_ref_blob_main
+
+
+int make_obs_blob_main(int argc, char* argv[static argc + 1]) {
+    if (argc < 2) {
+        fprintf(stderr, "usage: %s reference.blob\n", argv[0]);
+        return EXIT_FAILURE;
+    } // if
+
+    errno = 0;
+    FILE *stream = fopen(argv[1], "r");
+    if (stream == NULL) {
+        fprintf(stderr, "error: %s\n", strerror(errno));
+        return EXIT_FAILURE;
+    } // if
+
+    GVA_String reference = gva_fasta_sequence_blob(gva_std_allocator, stream);
+    fclose(stream);
+    fprintf(stderr, "reference length: %zu\n", reference.len);
+
+    GVA_Variant * variants = NULL;
+    GVA_Variant * lss = NULL;
+
+    size_t last = 0;
+    size_t line_count = 0;
+    size_t dropped = 0;
+    size_t distance = 0;
+    static char line[LINE_SIZE] = {0};
+    while (fgets(line, sizeof(line), stdin) != NULL) {
+        size_t const len = strlen(line) - 1;
+        GVA_Variant variant;
+        if (gva_parse_spdi(len, line, &variant) == 0) {
+            fprintf(stderr, "error: SPDI parsing failed at line %zu: %s", line_count + 1, line);
+            continue;
+        } // if
+
+        GVA_Variant trimmed = prefix_trimmed(reference.len, reference.str, variant);
+
+        if (array_length(variants) > 0 && trimmed.start < variants[array_length(variants) - 1].end) {
+            //fprintf(stderr, "dropped: at line %zu: %s", line_count + 1, line);
+            dropped += 1;
+            continue;
+        } // if
+
+        ARRAY_APPEND(gva_std_allocator, variants, gva_variant_dup(gva_std_allocator, trimmed));
+        line_count += 1;
+    } // while
+
+    fprintf(stderr, "===INPUT===\n");
+    fprintf(stderr, "#variants: %zu\n", array_length(variants));
+    fprintf(stderr, "#dropped:  %zu\n", dropped);
+
+    GVA_String observed = gva_patch(gva_std_allocator, reference.len, reference.str, array_length(variants), variants);
+    GVA_String lss_observed = gva_patch(gva_std_allocator, reference.len, reference.str, array_length(lss), lss);
+
+    fasta_blob_write(stdout, observed);
+
+    return EXIT_SUCCESS;
+} // make_obs_blob_main
+
+
 int
 main(int argc, char* argv[static argc + 1])
 {
@@ -1373,4 +1454,6 @@ main(int argc, char* argv[static argc + 1])
     // return dbsnp_main(argc, argv);
     // return locals_main(argc, argv);
     // return extract_main(argc, argv);
+    // return make_ref_blob_main(argc, argv);
+    // return make_obs_blob_main(argc, argv);
 } // main
