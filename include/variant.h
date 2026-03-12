73cca6e21578d65c2613386300b8dc3a3a4e53e8
Mark Santcroos <m.a.santcroos@lumc.nl>
2026-03-12 01:31:36 +0100

    Macros for allele parts.

--- include/variant.h
+++ include/variant.h
@@ -16,6 +16,8 @@
 #define GVA_VARIANT_PRINT_SPDI(ref_id, variant) ref_id, variant.start, (variant.end - variant.start), GVA_STRING_PRINT(variant.sequence)
 #define GVA_VARIANT_FMT_SPDI "%s:" GVA_UINT_FMT ":" GVA_UINT_FMT ":" GVA_STRING_FMT
 
+#define GVA_VARIANT_PRINT_SPDI_ALLELE(variant) variant.start, (variant.end - variant.start), GVA_STRING_PRINT(variant.sequence)
+#define GVA_VARIANT_FMT_SPDI_ALLELE GVA_UINT_FMT ":" GVA_UINT_FMT ":" GVA_STRING_FMT
 
 typedef struct
 {
